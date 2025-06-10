8a980aec2113f30239595a8c2d47254a097706cc
vherawidatama <vherawidatama@gmail.com>
2025-06-10 08:00:00 +0000

    sm76xx-common: add device version by SMGReborn
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,7 @@ TW_NO_EXFAT_FUSE := true
 TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_RESETPROP := true
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
+TW_DEVICE_VERSION := Amethyst by SMGReborn
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.fingerprint=ro.vendor.build.fingerprint;ro.build.version.incremental"
 RECOVERY_LIBRARY_SOURCE_FILES += \
